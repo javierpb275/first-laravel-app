e91582229c1e917ee4513611b077aced4b71a7d3
javierpb275 <javierpb275@gmail.com>
2020-12-28 14:43:44 +0100

    we check if the key exists and if not we will get a 404.
    so if did for example:
    http://127.0.0.1:8000/posts/third-post
    we get 404 Not Found.
    
    BUT if we do /first-post:
    It would still work as before vbecause that key DOES exist.

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,12 @@ Route::get('/posts/{post}', function ($post) {
         'second-post' => 'And this is my second blog post!',
     ];
 
+    if (!array_key_exists($post, $posts)) {
+        abort(404, 'Sorry, that post was not found');
+    }
+
     return view('post', [
-       'post' => $posts[$post] ?? 'Nothing here yet'
+       'post' => $posts[$post]
     ]);
 });
 
