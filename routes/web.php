317c471335714ce08a776ea02003e563cb2e7de6
javierpb275 <javierpb275@gmail.com>
2020-12-28 14:38:09 +0100

    we can provide a default in case we put in the url sth that doesn't exist like /third-blog so we would get Nothing here yet on the page.

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/posts/{post}', function ($post) {
     ];
 
     return view('post', [
-       'post' => $posts[$post]
+       'post' => $posts[$post] ?? 'Nothing here yet'
     ]);
 });
 
