0f35e3e99f8a15f6ef15e78737bd0f445f911216
javierpb275 <javierpb275@gmail.com>
2020-12-28 13:19:15 +0100

    we can just return a string in our homepage

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hello World";
 });
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
