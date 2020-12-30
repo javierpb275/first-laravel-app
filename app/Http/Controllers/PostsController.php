0f5fa8d44b79922fabe9e0cb3cdf21fe69ab3d37
javierpb275 <javierpb275@gmail.com>
2020-12-30 14:34:25 +0100

    created database and posts table and created a post.
    connected that databse with our laravel app.
    
    slug is one of the columns that has the messagge 'my-first-post' that we pass that in the url at the end.
    
    $post->body :
    we do this because is an object and we wanna access the body column that has text that is what we will see on our page:
    
    http://127.0.0.1:8000/posts/my-first-post#

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,19 +4,12 @@ namespace App\Http\Controllers;
 
 class PostsController 
 {
-    public function show($post)
+    public function show($slug)
     {     
-            $posts = [
-                'first-post' => 'Hello, this is my first blog post!',
-                'second-post' => 'And this is my second blog post!',
-            ];
-        
-            if (!array_key_exists($post, $posts)) {
-                abort(404, 'Sorry, that post was not found');
-            }
+            $post = \DB::table('posts')->where('slug', $slug)->first();
         
             return view('post', [
-               'post' => $posts[$post]
+               'post' =>  $post
             ]); 
     }
 }
