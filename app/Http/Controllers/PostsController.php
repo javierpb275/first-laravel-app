3b33229a0cc6a87df518d23acda4b44bbe9e638d
javierpb275 <javierpb275@gmail.com>
2020-12-30 16:46:02 +0100

    created Post model and changed code in the controller so we access that model and it is a little bit cleaner than before.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class PostsController 
 {
     public function show($slug)
     {     
-            $post = \DB::table('posts')->where('slug', $slug)->first();
+
+            $post = Post::where('slug', $slug)->first();
 
             if (!$post) {
                 abort(404);
