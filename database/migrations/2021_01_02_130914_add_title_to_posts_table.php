5c28fbcb54a15f9ba612e8c11adb798e829a7fde
javierpb275 <javierpb275@gmail.com>
2021-01-02 14:15:53 +0100

    added a few columns.
    ->nullable();: this is the way of saying that is optional.
    
    up(): run the migration.
    
    down(): roll back or undo the migration.
    
    ---------------------
    
    and also used this command:
    
    php artisan make:migration add_title_to_posts_table
    
    to make the migration add_tittle_to_posts_table to add a column to the posts table called title.

--- database/migrations/2021_01_02_130914_add_title_to_posts_table.php
+++ database/migrations/2021_01_02_130914_add_title_to_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class AddTitleToPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->string('slug');
-            $table->text('body');
-            $table->timestamps();
-            $table->timestamp('published_at')->nullable();
+        Schema::table('posts', function (Blueprint $table) {
+            //
         });
     }
 
@@ -29,6 +25,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 }
