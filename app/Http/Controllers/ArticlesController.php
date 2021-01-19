5826e15c76600c87adcb3c72bbc9ce8821117c0f
javierpb275 <javierpb275@gmail.com>
2021-01-19 13:42:45 +0100

    The Seven RestFul Controller actions: 1. index; 2. show; 3. create; 4. store; 5. edit; 6. update; 7. destroy.

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -8,14 +8,51 @@ use Illuminate\Http\Request;
 class ArticlesController extends Controller
 {
     public function index() {
+
+        //Renders a list of a resource
+
         $articles = Article::latest()->get();
 
         return view('articles.index', ['articles' => $articles]);
     }
 
     public function show($id) {
+
+        //Shows a single resource
+
         $article = Article::find($id);
 
         return view('articles.show', ['article' => $article]);
     }
+
+    public function create() {
+
+        //Shows a view to create a new resource
+       
+    }
+
+    public function store() {
+
+        //Persists the new resource created
+        
+    }
+
+    public function edit() {
+
+        //Shows a view to edit an existing resource
+
+    }
+
+    public function update() {
+
+        //Persists he edited resource
+
+    }
+
+    public function destroy() {
+
+        //Delete the resource
+
+    }
+
 }
