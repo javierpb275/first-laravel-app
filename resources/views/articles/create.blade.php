00c4cfe31822a31fcd252795b31b05bd97772c1c
javierpb275 <javierpb275@gmail.com>
2021-01-21 19:13:32 +0100

    A cleaner way of handling the error in all the fields.
    When we left the fields empty the inputs will go red and it will pop up a message below saying: tile required (in red)

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -17,10 +17,10 @@
                     <label class="label" for="title">Title</label>
 
                     <div class="control">
-                        <input class="input {{$errors->has('title') ? 'is-danger' : ''}}" type="text" name="title" id="title">
-                        @if ($errors->has('title'))
+                        <input class="input @error('title') is-danger @enderror" type="text" name="title" id="title">
+                        @error ('title')
                         <p class="help is-danger">{{$errors->first('title')}}</p>
-                        @endif
+                        @enderror
                     </div>
 
                 </div>
@@ -30,7 +30,10 @@
                     <label class="label" for="excerpt">Excerpt</label>
 
                     <div class="control">
-                        <textarea class="textarea"  name="excerpt" id="excerpt"></textarea>
+                        <textarea class="textarea @error('excerpt') is-danger @enderror"  name="excerpt" id="excerpt"></textarea>
+                        @error ('excerpt')
+                        <p class="help is-danger">{{$errors->first('excerpt')}}</p>
+                        @enderror
                     </div>
 
                 </div>
@@ -40,7 +43,10 @@
                     <label class="label" for="body">Body</label>
 
                     <div class="control">
-                        <textarea class="textarea"  name="body" id="body"></textarea>
+                        <textarea class="textarea @error('body') is-danger @enderror"  name="body" id="body"></textarea>
+                        @error ('body')
+                        <p class="help is-danger">{{$errors->first('body')}}</p>
+                        @enderror
                     </div>
 
                 </div>
